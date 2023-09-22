7038b6f9d1c2db51ff2127d5ebd60e860ba23280
BryanR294 <bryanareval@gmail.com>
2023-09-22 12:00:59 -0400

    Near Final Version of Super-Mass star cycle

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 class Star{
-    constructor(name, DOM){
+    constructor(name, DOM, description){
         this.name = name;
         this.DOM = DOM;
+        this.description = description;
     }
 }
 
-const Nebula = new Star("Nebula", document.getElementById("nebula"));
-const Protostar = new Star("Protostar", document.getElementById("protostar"));
-const OrangeStar = new Star("Star", document.getElementById("orange-star"));
-const Supergiant = new Star("Supergiant", document.getElementById("supergiant"));
-const Supernova = new Star("Supernova", document.getElementById("supernova"));
-const Blackhole = new Star("Blackhole", document.getElementById("blackhole"));
+const Nebula = new Star("Nebula", document.getElementById("nebula"), "The Nebula is a star in the making, its basic elements will eventually fuse together to create a star.");
+const Protostar = new Star("Protostar", document.getElementById("protostar"), "Gravity begins to condense all the elements together in this stage.");
+const OrangeStar = new Star("Star", document.getElementById("orange-star"),"Gravity condensed the elements to create this star. Due to this star's mass, this star will be bigger than other stars and a little cooler (hence its oranger color).");
+const Supergiant = new Star("Supergiant", document.getElementById("supergiant"),"Helium and Hydrogren began fusing into heavier elements such as iron, this process causes the star's surface to expand as well as turning red.");
+const Supernova = new Star("Supernova", document.getElementById("supernova"),"When a star reaches the end of its life cycle, it's core will collapse and the star explodes. This is what we call a supernova.");
+const Blackhole = new Star("Blackhole", document.getElementById("blackhole"),"Due to its immense mass, we are left with a blackhole. The densitiy causes the blackhole's gravity doesn't allow anything to escape it, including light.");
 // let starEl = document.getElementById("yellow-star");
 // let redGiantEl = document.getElementById("red-giant");
 // let neutronStarEl = document.getElementById("neutron-star");
@@ -69,4 +70,26 @@ function updateInformation(Star){
     nameEl.textContent = Star.name;
     counter = imageIndex + 1;
     stageEl.textContent = "Stage " + counter;
+}
+
+function displayInfo(){
+    let containerEl = document.getElementById("info-container");
+    let buttonEl = document.getElementById("info-btn");
+    let textEl = document.getElementById("info-text");
+    if(containerEl.style.height == "10%"){
+        textEl.innerHTML = SUPER_MASS_STARS[imageIndex].description;
+        buttonEl.innerHTML = "v";
+        buttonEl.style.display = "block";
+        buttonEl.style.top = "5%";
+        buttonEl.style.height = "10%";
+        containerEl.style.height = "60%";
+        containerEl.style.backgroundColor = "black";
+        textEl.style.display = "flex";
+    }
+    else{
+        buttonEl.innerHTML = "^";
+        containerEl.style.height = "10%";
+        containerEl.style.backgroundColor = "rgba(0,0,0,.2)";
+        textEl.style.display = "none";
+    }
 }
